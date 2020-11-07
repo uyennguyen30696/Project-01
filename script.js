d358ec5330ba199013acbdd1bd517b8465005fad
Uyen Nguyen <udnguyen@ucsd.edu>
2020-11-07 13:33:09 -0800

    add map demonstration - not working yet

--- script.js
+++ script.js
@@ -4,15 +4,17 @@
 // Openrouteservice API key
 
 function openRouteMap() {
-    var APIKey = "5b3ce3597851110001cf624823016920625e4e46933015e7a19f69e6";
-    var queryURL = "https://api.openrouteservice.org/v2/directions/driving-car?api_key=" + APIKey + "&start=8.681495,49.41461" + "&end=8.687872,49.420318";
+  var APIKey = "5b3ce3597851110001cf624823016920625e4e46933015e7a19f69e6";
+  var queryURL = "https://api.openrouteservice.org/v2/directions/driving-car?api_key=" + APIKey + "&start=8.681495,49.41461" + "&end=8.687872,49.420318";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-      });
-  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    console.log(response.metadata.query.profile) // Driving-car
+    
+  });
+
 }
 openRouteMap();
